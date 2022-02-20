64cad552c748a141a8751be8a433347feca143f2
Kunal3723 <78958213+Kunal3723@users.noreply.github.com>
2022-02-20 10:27:41 +0530

    Update restApi.js

--- src/api/restApi.js
+++ src/api/restApi.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/videos';
+//for local
+//const url = 'http://localhost:5000/videos';
+
+const url = 'https://youtube-fetch.herokuapp.com/videos';
 
 export const fetchVideos = (page) => axios.get(`${url}?page=${page}&limit=9`);
 export const createVideo = (newVideo) => axios.post(url, newVideo);
